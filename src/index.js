6c3fdfd3f352f2e3fb7add6f74fc13d81f70e6ac
ashmit <ashmitbastola@gmail.com>
2025-04-17 18:42:46 +0545

    deployment test

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
 origin:process.env.CLIENT_URI,
-methods:["GET", "POST", "PUT", "DELETE"],
+methods:["GET", "POST", "PUT", "DELETE","PATCH","OPTIONS"],
 allowedHeaders: ["Content-Type", "Authorization"],
 credentials:true
 }))
