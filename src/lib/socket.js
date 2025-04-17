7dc35e55e19d591635b7c1c8e4d0188c7f00edad
ashmit <ashmitbastola@gmail.com>
2025-04-17 18:10:23 +0545

    deployment test

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,7 +1,9 @@
 import {Server} from 'socket.io';
 import http from 'http'
 import express from 'express';
+import { configDotenv } from 'dotenv';
 
+configDotenv()
 
 const app=express()
 const server = http.createServer(app);
